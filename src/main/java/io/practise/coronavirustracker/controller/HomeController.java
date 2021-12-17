01bb8bb8287f8cebddc081ec3dbd26cb25fd6c3b
pushpad01 <cse.pushpad1@gmail.com>
2021-12-17 13:37:22 +0530

    html page added and cases updated every day

---
+++ src/main/java/io/practise/coronavirustracker/controller/HomeController.java
@@ -0,0 +1,21 @@
+package io.practise.coronavirustracker.controller;
+
+import io.practise.coronavirustracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
+        return "home";
+    }
+
+}
